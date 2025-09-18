7fc5e5539f2f54ffad6a7a697d650b7eafcdf751
Jim <jimsamuel50@gmail.com>
2025-09-18 07:03:17 +0100

    remove memtable . MTable Logic

--- src/memtable/memtable.rs
+++ src/memtable/memtable.rs
@@ -2,7 +2,7 @@ pub mod memtable {
     use std::io::Result;
     use std::sync::{Arc, RwLock};
     use std::collections::BTreeMap;
-    use std::path::PathBuf;
+    use std::path::{Path, PathBuf};
     use SDB::lsm::{WAL,WNode};
 
     pub struct MTable {
@@ -15,7 +15,7 @@ pub mod memtable {
             let wal = WAL::replay_two(dir.into());
             wal
         }
-        pub fn put(&self, key: &str, val: &str) {
+        pub fn put(&self, key: &str, val: &str){
             let wnode = WNode::new(key, val);
             let mut dat = self.DBuf.write().unwrap();
             dat.insert(wnode.key.into(), wnode.val);
